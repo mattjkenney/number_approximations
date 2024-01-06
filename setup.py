0020a73c0310c77f374a0aae3735fab2a8655a08
mattjkenney <86480338+mattjkenney@users.noreply.github.com>
2024-01-06 17:59:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
 
+setup(
+    name = 'Number Approximations',
+    packages = ['number_approximations'],
+    version = '1.0.0-alpha',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Matthew J. Kenney',
+    author_email = '',
+    url = 'github package source url',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
